18d4bf2081bb8d66c66f00809a623a11e4126572
Anandhu <anandhusj864@gmail.com>
2022-03-24 20:46:26 +0530

    Second Commit

--- TuitionCenter/urls.py
+++ TuitionCenter/urls.py
@@ -26,4 +26,29 @@ urlpatterns = [
 
     re_path(r'^$', views.Index_Admin, name='Index_Admin'),
     re_path(r'^Registration_Admin$', views.Registration_Admin, name='Registration_Admin'),
+    re_path(r'^RegistrationStaff_Admin$', views.RegistrationStaff_Admin, name='RegistrationStaff_Admin'),
+    re_path(r'^RegistrationCurrentStaff_Admin$', views.RegistrationCurrentStaff_Admin, name='RegistrationCurrentStaff_Admin'),
+    re_path(r'^RegistrationResignedStaff_Admin$', views.RegistrationResignedStaff_Admin, name='RegistrationResignedStaff_Admin'),
+
+    re_path(r'^Staff_Admin$', views.Staff_Admin, name='Staff_Admin'),
+    re_path(r'^StaffCurrentstaff_Admin$', views.StaffCurrentstaff_Admin, name='StaffCurrentstaff_Admin'),
+    re_path(r'^StaffPreviousstaff_Admin$', views.StaffPreviousstaff_Admin, name='StaffPreviousstaff_Admin'),
+    re_path(r'^StaffCurrentstaffProfile_Admin$', views.StaffCurrentstaffProfile_Admin, name='StaffCurrentstaffProfile_Admin'),
+    re_path(r'^StaffPreviousstaffProfile_Admin$', views.StaffPreviousstaffProfile_Admin, name='StaffPreviousstaffProfile_Admin'),
+    re_path(r'^StaffCurrentstaffAttendance_Admin$', views.StaffCurrentstaffAttendance_Admin, name='StaffCurrentstaffAttendance_Admin'),
+    re_path(r'^StaffCurrentstaffAttendanceSort_Admin$', views.StaffCurrentstaffAttendanceSort_Admin, name='StaffCurrentstaffAttendanceSort_Admin'),
+    re_path(r'^StaffPreviousstaffAttendance_Admin$', views.StaffPreviousstaffAttendance_Admin, name='StaffPreviousstaffAttendance_Admin'),
+    re_path(r'^StaffPreviousstaffAttendanceSort_Admin$', views.StaffPreviousstaffAttendanceSort_Admin, name='StaffPreviousstaffAttendanceSort_Admin'),
+    
+    re_path(r'^Student_Admin$', views.Student_Admin, name='Student_Admin'),
+    re_path(r'^StudentCurrentstudent_Admin$', views.StudentCurrentstudent_Admin, name='StudentCurrentstudent_Admin'),
+    re_path(r'^StudentPreviousstudent_Admin$', views.StudentPreviousstudent_Admin, name='StudentPreviousstudent_Admin'),
+
+    re_path(r'^Academic_Admin$', views.Academic_Admin, name='Academic_Admin'),
+    re_path(r'^AcademicBatch_Admin$', views.AcademicBatch_Admin, name='AcademicBatch_Admin'),
+    re_path(r'^AcademicAddBatch_Admin$', views.AcademicAddBatch_Admin, name='AcademicAddBatch_Admin'),
+    re_path(r'^AcademicAddBatchUpdate_Admin$', views.AcademicAddBatchUpdate_Admin, name='AcademicAddBatchUpdate_Admin'),
+    re_path(r'^AcademicViewBatch_Admin$', views.AcademicViewBatch_Admin, name='AcademicViewBatch_Admin'),
+    
 ]
+
--- app/views.py
+++ app/views.py
@@ -16,4 +16,72 @@ def Index_Admin(request):
        return render(request, 'Index_Admin.html')
 
 def Registration_Admin(request):
-       return render(request, 'Registration_Admin.html')       
+       return render(request, 'Registration_Admin.html')    
+
+def RegistrationStaff_Admin(request):
+       return render(request, 'RegistrationStaff_Admin.html')      
+
+def RegistrationCurrentStaff_Admin(request):
+       return render(request, 'RegistrationCurrentStaff_Admin.html')     
+
+def RegistrationResignedStaff_Admin(request):
+       return render(request, 'RegistrationResignedStaff_Admin.html')                       
+
+
+
+def Staff_Admin(request):
+       return render(request, 'Staff_Admin.html') 
+
+def StaffCurrentstaff_Admin(request):
+       return render(request, 'StaffCurrentstaff_Admin.html') 
+
+def StaffPreviousstaff_Admin(request):
+       return render(request, 'StaffPreviousstaff_Admin.html') 
+
+def StaffCurrentstaffProfile_Admin(request):
+       return render(request, 'StaffCurrentstaffProfile_Admin.html') 
+
+def StaffPreviousstaffProfile_Admin(request):
+       return render(request, 'StaffPreviousstaffProfile_Admin.html') 
+
+def StaffCurrentstaffAttendance_Admin(request):
+       return render(request, 'StaffCurrentstaffAttendance_Admin.html')  
+
+def StaffCurrentstaffAttendanceSort_Admin(request):
+       return render(request, 'StaffCurrentstaffAttendanceSort_Admin.html')             
+
+def StaffPreviousstaffAttendance_Admin(request):
+       return render(request, 'StaffPreviousstaffAttendance_Admin.html') 
+
+def StaffPreviousstaffAttendanceSort_Admin(request):
+       return render(request, 'StaffPreviousstaffAttendanceSort_Admin.html')            
+
+
+
+def Student_Admin(request):
+       return render(request, 'Student_Admin.html') 
+
+def StudentCurrentstudent_Admin(request):
+       return render(request, 'StudentCurrentstudent_Admin.html') 
+
+def StudentPreviousstudent_Admin(request):
+       return render(request, 'StudentPreviousstudent_Admin.html') 
+
+
+def Academic_Admin(request):
+       return render(request, 'Academic_Admin.html') 
+
+def AcademicBatch_Admin(request):
+       return render(request, 'AcademicBatch_Admin.html')
+
+def AcademicAddBatch_Admin(request):
+       return render(request, 'AcademicAddBatch_Admin.html') 
+
+def AcademicAddBatchUpdate_Admin(request):
+       return render(request, 'AcademicAddBatchUpdate_Admin.html') 
+
+
+def AcademicViewBatch_Admin(request):
+       return render(request, 'AcademicViewBatch_Admin.html') 
+
+       
